fe27606e9f371846bd2a22394aa6938899712cca
ramkie05 <82752269+ramkie05@users.noreply.github.com>
2021-04-18 15:19:05 +0530

    Create Account.cs

---
+++ GunvorAssessment/Account/Account.cs
@@ -0,0 +1,10 @@
+using System.Threading.Tasks;
+
+namespace GunvorAssessment.Account
+{
+public class Account : IAccount
+{
+	public async Task DepositAsync(decimal amount){}
+	public async Task WithdrawAsync(decimal amount){}
+}
+}
